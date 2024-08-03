5f15b4c117767c2e4da6a251c655dcb1bacb6f39
vietanhcao <vietanhcao1994@gmail.com>
2024-08-03 23:14:56 +0700

    feat(layout): update dark mode

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { ModeToggle } from "../../../components/ModeToggle";
 
 export default function LoginPage() {
-	return <div>LoginPage</div>;
+	return (
+		<div>
+			<ModeToggle /> 
+			<h4>Login Page</h4>
+		</div>
+	);
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 
 import "./globals.css";
+import { ThemeProvider } from "../components/theme-provider";
 
 // const roboto = Roboto({
 // 	subsets: ["vietnamese"],
@@ -36,10 +37,17 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en" className={`${myFont.className} ${myFont.variable}`}>
+		<html lang="en" className={`${myFont.className} ${myFont.variable}`} suppressHydrationWarning>
 			<body>
 				<header>my root header</header>
-				{children}
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+					disableTransitionOnChange
+				>
+					{children}
+				</ThemeProvider>
 			</body>
 		</html>
 	);
